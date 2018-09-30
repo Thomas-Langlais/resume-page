ab21868fc92ff508f7119ac21cabb68473b11b86
Thomas L'Anglais <tlang072@uottawa.ca>
2018-09-30 19:04:02 -0400

    getting cards to grow up and down

--- src/components/card.js
+++ src/components/card.js
@@ -3,25 +3,33 @@ import '../css/card.css';
 import '../css/utils.css';
 
 export default class Card extends React.Component {
+    
     constructor(props) {
         super(props);
         this.state = {
-            isFocused: false
+            isFocused: false,
+            location: {
+                top: 0,
+                left: 0
+            }
         }
 
         this.expandCard = this.expandCard.bind(this);
     }
 
+    componentDidMount() {
+        
+    }
+
     expandCard() {
-        this.setState(state => ({
-            isFocused: !state.isFocused
-        }));
+        this.setState(state => (Object.assign(state, {isFocused: !state.isFocused})));
     }
 
     // state change isExpanded, set state and change state on btn click
     render() {
+        const shouldExpand = this.state.isFocused;
         return (
-            <div className="card">
+            <div ref={(el) => this._self = el}className={"card" + (shouldExpand ? " expanded" : "")}>
                 <div id="cd-title" className="title">
                     {this.props.title}
                 </div>
@@ -30,7 +38,7 @@ export default class Card extends React.Component {
                     {this.props.children}
                 </div>
                 <button id="cd-btn" onClick={this.expandCard} className="meshed-btn">
-                    <i class="fas fa-chevron-down"></i>
+                    <i className="fas fa-chevron-down"></i>
                 </button>
             </div>
         )
