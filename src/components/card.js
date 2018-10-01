6f5a2c47cb5af554f520de21a14b94eaaf01e2ed
Thomas L'Anglais <thomas1446@gmail.com>
2018-10-01 00:00:59 -0400

    card resizing

--- src/components/card.js
+++ src/components/card.js
@@ -15,21 +15,47 @@ export default class Card extends React.Component {
         }
 
         this.expandCard = this.expandCard.bind(this);
+        this.updateDims = this.updateDims.bind(this);
+        this.debug = this.debug.bind(this);
+    }
+
+    debug() {
+        console.log(this);
+    }
+    expandCard() {
+        this.setState(state => (Object.assign(state, {isFocused: !state.isFocused})));
+    }
+
+    updateDims() {
+        let rect = this._self.getBoundingClientRect();
+        this.setState(state => {Object.assign(state, {
+            location: { 
+                top: rect.top,
+                left: rect.left,
+                width: rect.width,
+                height: rect.height
+            }
+        })});
+    }
+
+    componentWillMount() {
+        
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDims);
     }
 
     componentDidMount() {
-        this.state.location.top = this._self
-    }
-
-    expandCard() {
-        this.setState(state => (Object.assign(state, {isFocused: !state.isFocused})));
+        this.updateDims();
+        window.addEventListener('resize', this.updateDims);
     }
 
     // state change isExpanded, set state and change state on btn click
     render() {
         const shouldExpand = this.state.isFocused;
         return (
-            <div ref={(el) => this._self = el}className={"card" + (shouldExpand ? " expanded" : "")}>
+            <div onClick={this.debug} ref={(el) => this._self = el}className={"card" + (shouldExpand ? " expanded" : "")}>
                 <div id="cd-title" className="title">
                     {this.props.title}
                 </div>
